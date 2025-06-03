a78858b40757254628747541efddd0c76189046e
SonamYangchen77 <07230064.sherubtse@rub.edu.bt>
2025-06-03 12:11:06 +0600

    personal

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const createUsersTableIfNotExists = async () => {
       id SERIAL PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
+      password VARCHAR(255) NOT NULL,
+      is_verified BOOLEAN DEFAULT false
     );
   `);
   console.log('✅ Ensured users table exists');
